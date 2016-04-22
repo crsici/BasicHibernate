52f439cfb1644aa544e0e969d730f689cbbb424a
trungp <trung.pham@gbst.net>
2016-04-22 17:37:24 +0700

    Init

---
+++ HelloHibernate/src/main/java/model/Dummy.java
@@ -0,0 +1,14 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * Created by pthanhtrung on 4/21/2016.
+ */
+@Entity
+public class Dummy {
+    @Id
+    Integer id;
+
+}
--- HelloHibernate/src/test/java/SimpleHibernate.java
+++ HelloHibernate/src/test/java/SimpleHibernate.java
@@ -1,20 +1,58 @@
+import model.Dummy;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
 import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  * Created by pthanhtrung on 4/20/2016.
  */
 public class SimpleHibernate {
-    SessionFactory factory;
+    public static SessionFactory factory;
 
     @BeforeClass
-    public void setup(){
+    public static void setup() throws ClassNotFoundException, IllegalAccessException, InstantiationException, SQLException {
+        try {
+            Class.forName("org.hsqldb.jdbcDriver").newInstance();
+        } catch (Exception e) {
+            System.out.println("ERROR: failed to load HSQLDB JDBC driver.");
+            e.printStackTrace();
+            return;
+        }
+
+
         Configuration configuration = new Configuration();
-        configuration.configure();
+        configuration.addProperties(getHibernateProperties());
+        configuration.addAnnotatedClass(Dummy.class);
+
         factory = configuration.buildSessionFactory();
+
+
     }
 
+    public static Properties getHibernateProperties() {
+
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
+        properties.put("connection.driver_class", "org.hsqldb.jdbc.JDBCDriver");
+        properties.put("hibernate.connection.url", "jdbc:hsqldb:mem:test;shutdown=true");
+        properties.put("hibernate.show_sql", true);
+        properties.put("hibernate.hbm2ddl.auto", "create-drop");
+
+
+        return properties;
+    }
+
+    @Test
+    public void initHibernate(){
+        Session  session = factory.openSession();
+        session.save(new Dummy());
+    }
 
 }
